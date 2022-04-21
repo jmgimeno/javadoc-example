b95f3e9ddf8df68304b4171ffbee5445c9d1c3d9
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2022-04-21 07:59:35 +0200

    updated example

--- src/example/Student.java
+++ src/example/Student.java
@@ -12,81 +12,91 @@ package example;
 
 public class Student {
 
-/**
- * Creates a new Student object with the specified name and ID.
- * @param name The student's name as a String
- * @param id The student's ID number as an int
- */
-	public Student(String name, int id) {
-		studentName = name;
-		studentID = id;
-	}
+    /**
+     * Creates a new Student object with the specified name and ID.
+     *
+     * @param name The student's name as a String
+     * @param id   The student's ID number as an int
+     */
+    public Student(String name, int id) {
+        studentName = name;
+        studentID = id;
+    }
 
-/**
- * Gets the name of this student.
- * @return The name of this student
- */
-	public String getName() {
-		return studentName;
-	}
+    /**
+     * Gets the name of this student.
+     *
+     * @return The name of this student
+     */
+    public String getName() {
+        return studentName;
+    }
 
-/**
- * Gets the ID number of this student.
- * @return The ID number of this student
- */
-	public int getID() {
-		return studentID;
-	}
+    /**
+     * Gets the ID number of this student.
+     *
+     * @return The ID number of this student
+     */
+    public int getID() {
+        return studentID;
+    }
 
-/**
- * Sets the number of credits earned.
- * @param credits The new number of credits earned
- */
-	public void setCredits(double credits) {
-		creditsEarned = credits;
-	}
+    /**
+     * Sets the number of credits earned.
+     *
+     * @param credits The new number of credits earned
+     */
+    public void setCredits(double credits) {
+        creditsEarned = credits;
+    }
 
-/**
- * Gets the number of credits earned.
- * @return The number of credits this student has earned
- */
-	public double getCredits() {
-		return creditsEarned;
-	}
+    /**
+     * Gets the number of credits earned.
+     *
+     * @return The number of credits this student has earned
+     */
+    public double getCredits() {
+        return creditsEarned;
+    }
 
-/**
- * Sets whether the student is paid up.
- * @param flag The value true or false indicating paid-up status
- */
-	public void setPaidUp(boolean flag) {
-		paidUp = flag;
-	}
+    /**
+     * Sets whether the student is paid up.
+     *
+     * @param flag The value true or false indicating paid-up status
+     */
+    public void setPaidUp(boolean flag) {
+        paidUp = flag;
+    }
 
-/**
- * Returns whether the student is paid up.
- * @return Whether the student is paid up
- */
-	public boolean isPaidUp() {
-		return paidUp;
-	}
+    /**
+     * Returns whether the student is paid up.
+     *
+     * @return Whether the student is paid up
+     */
+    public boolean isPaidUp() {
+        return paidUp;
+    }
 
-/**
- * Creates a string identifying this student.
- * @return The string used to display this student
- */
-	public String toString() {
-		return studentName + " (#" + studentID + ")";
-	}
+    /**
+     * Creates a string identifying this student.
+     *
+     * @return The string used to display this student
+     */
+    public String toString() {
+        return studentName + " (#" + studentID + ")";
+    }
 
-/* Public constants */
+    /* Public constants */
 
-/** The number of credits required for graduation */
-	public static final double CREDITS_TO_GRADUATE = 32.0;
+    /**
+     * The number of credits required for graduation
+     */
+    public static final double CREDITS_TO_GRADUATE = 32.0;
 
-/* Private instance variables */
-	private String studentName;   /* The student's name           */
-	private int studentID;        /* The student's ID number      */
-	private double creditsEarned; /* The number of credits earned */
-	private boolean paidUp;       /* Whether student is paid up   */
+    /* Private instance variables */
+    private final String studentName;   /* The student's name           */
+    private final int studentID;        /* The student's ID number      */
+    private double creditsEarned; /* The number of credits earned */
+    private boolean paidUp;       /* Whether student is paid up   */
 
 }
--- src/example/StudentMain.java
+++ src/example/StudentMain.java
@@ -10,31 +10,31 @@ import acm.program.CommandLineProgram;
 
 public class StudentMain extends CommandLineProgram {
 
-	public static void main(String[] args) {
-		new StudentMain().start(args);
-	}
+    public static void main(String[] args) {
+        new StudentMain().start(args);
+    }
 
-	public void run() {
-		Student diligentStudent = new Student("Hermione Granger", 314159);
-		diligentStudent.setCredits(97);
-		diligentStudent.setPaidUp(true);
-		Student weakStudent = new Student("Vincent Crabbe", 271828);
-		weakStudent.setCredits(17.5);
-		weakStudent.setPaidUp(true);
-		Student impoverishedStudent = new Student("Ron Weasley", 161803);
-		impoverishedStudent.setCredits(32);
-		testEligibility(diligentStudent);
-		testEligibility(weakStudent);
-		testEligibility(impoverishedStudent);
-	}
+    public void run() {
+        Student diligentStudent = new Student("Hermione Granger", 314159);
+        diligentStudent.setCredits(97);
+        diligentStudent.setPaidUp(true);
+        Student weakStudent = new Student("Vincent Crabbe", 271828);
+        weakStudent.setCredits(17.5);
+        weakStudent.setPaidUp(true);
+        Student impoverishedStudent = new Student("Ron Weasley", 161803);
+        impoverishedStudent.setCredits(32);
+        testEligibility(diligentStudent);
+        testEligibility(weakStudent);
+        testEligibility(impoverishedStudent);
+    }
 
-	private void testEligibility(Student student) {
-		String verb = (isEligibleToGraduate(student)) ? "is" : "is not";
-		println(student + " " + verb + " eligible to graduate.");
-	}
+    private void testEligibility(Student student) {
+        String verb = (isEligibleToGraduate(student)) ? "is" : "is not";
+        println(student + " " + verb + " eligible to graduate.");
+    }
 
-	private boolean isEligibleToGraduate(Student student) {
-		return student.getCredits() >= Student.CREDITS_TO_GRADUATE
-		       && student.isPaidUp();
-	}
+    private boolean isEligibleToGraduate(Student student) {
+        return student.getCredits() >= Student.CREDITS_TO_GRADUATE
+                && student.isPaidUp();
+    }
 }
